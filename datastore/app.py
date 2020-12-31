a1b1269f9471c6644b6994ef58c142c22f71234d
vsutar1451 <49795843+vsutar1451@users.noreply.github.com>
2020-12-31 23:44:19 +0530

    Update app.py

--- datastore/app.py
+++ datastore/app.py
@@ -1,4 +1,4 @@
-__author__ = 'prabin-sr'
+
 
 from sys import exit
 from flask import Flask
@@ -8,14 +8,11 @@ from utils.filehandler import FilePreprocess
 from CRD.views import CreateData, ReadData, DeleteData
 
 
-# Adding/Enabling CommandLineArguments: --datastore
 parser = ArgumentParser()
 parser.add_argument('--datastore', help='Enter the datastore absolute path.')
 args = parser.parse_args()
 
 
-# Selecting the DataStore Directory.
-# Select user provided datastore path else, select the default path.
 if args.datastore:
     db_path = args.datastore
 else:
@@ -31,17 +28,14 @@ if not directory_created:
 app = Flask(__name__)
 
 
-# Flask App Configurations
 app.config['DEBUG'] = settings.DEBUG
 app.config['SECRET_KEY'] = settings.SECRET_KEY
 
 
-# API Endpoints
 app.add_url_rule('/datastore/create', view_func=CreateData.as_view('create', db_path), methods=['POST'])
 app.add_url_rule('/datastore/read', view_func=ReadData.as_view('read', db_path), methods=['GET'])
 app.add_url_rule('/datastore/delete', view_func=DeleteData.as_view('delete', db_path), methods=['DELETE'])
 
 
-# Initiates Flask Server
 if __name__ == '__main__':
     app.run(host=settings.HOST, port=settings.PORT)
